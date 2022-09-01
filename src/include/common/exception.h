3ecaeee7dd1b4189eec175992592139d98da5f3f
Vik1ang <vik1ang@outlook.com>
2022-09-01 17:48:48 +0800

    init

---
+++ src/include/common/exception.h
@@ -0,0 +1,20 @@
+//===----------------------------------------------------------------------===//
+//
+//                         BusTub
+//
+// exception.h
+//
+// Identification: src/include/common/exception.h
+//
+// Copyright (c) 2015-2019, Carnegie Mellon University Database Group
+//
+//===----------------------------------------------------------------------===//
+
+#pragma once
+
+namespace bustub {
+
+// TODO(WAN): the comment I added below is a lie, but you shouldn't need to poke around here. Don't worry about it.
+//  Most of the exception types are type subsystem madness. I think we can get rid of it at some point.
+/** ExceptionType is all the types of exceptions that we expect to throw in our system. */
+}  // namespace bustub
---
+++ src/include/common/rwlatch.h
@@ -0,0 +1,22 @@
+//===----------------------------------------------------------------------===//
+//
+//                         BusTub
+//
+// rwmutex.h
+//
+// Identification: src/include/common/rwlatch.h
+//
+// Copyright (c) 2015-2019, Carnegie Mellon University Database Group
+//
+//===----------------------------------------------------------------------===//
+
+#pragma once
+
+namespace bustub {
+
+/**
+ * Reader-Writer latch backed by std::mutex
+ */
+class ReaderWriterLatch {};
+
+}  // namespace bustub
