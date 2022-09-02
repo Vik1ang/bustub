68ff0df2f01e8a4d2b190254852585d643af4f5c
Vik1ang <vik1ang@outlook.com>
2022-09-02 14:13:53 +0800

    string_util

--- src/common/util/string_util.cpp
+++ src/common/util/string_util.cpp
@@ -10,6 +10,9 @@
 //
 //===----------------------------------------------------------------------===//
 
+#include <algorithm>
+#include <cstdarg>
+#include <cstring>
 #include <iomanip>
 #include <sstream>
 
@@ -111,6 +114,105 @@ auto StringUtil::FormatSize(uint64_t bytes) -> std::string {
   }
 
   return (os.str());
+}
+
+auto StringUtil::Bold(const std::string &str) -> std::string {
+  std::string set_plain_text = "\033[0;0m";
+  std::string set_bold_text = "\033[0;1m";
+
+  std::ostringstream os;
+
+  os << set_bold_text << str << set_bold_text;
+
+  return (os.str());
+}
+
+auto StringUtil::Upper(const std::string &str) -> std::string {
+  std::string copy(str);
+  std::transform(copy.begin(), copy.end(), copy.begin(), [](unsigned char c) { return std::toupper(c); });
+
+  return (copy);
+}
+
+auto StringUtil::Lower(const std::string &str) -> std::string {
+  std::string copy(str);
+  std::transform(copy.begin(), copy.end(), copy.begin(), [](unsigned char c) { return std::tolower(c); });
+
+  return (copy);
+}
+
+// NOLINTNEXTLINE - it wants us to take fmt_str as const&, but we shouldn't do that since we use it in va_args.
+auto StringUtil::Format(std::string fmt_str, ...) -> std::string {
+  // http://stackoverflow.com/a/8098080
+  // Reserve two times as much as the length of the format string.
+  int final_n;
+  int n = (static_cast<int>(fmt_str.size())) * 2;
+  std::string str;
+  std::unique_ptr<char[]> formatted;
+  va_list ap;
+
+  while (true) {
+    // Wrap the plain char array into the unique_ptr
+    formatted = std::make_unique<char[]>(n);
+    strcpy(&formatted[0], fmt_str.c_str());
+    va_start(ap, fmt_str);
+    final_n = vsnprintf(&formatted[0], static_cast<size_t>(n), fmt_str.c_str(), ap);
+    va_end(ap);
+    if (final_n < 0 || final_n >= n) {
+      n += abs(final_n - n + 1);
+    } else {
+      break;
+    }
+  }
+  return {formatted.get()};
+}
+
+auto StringUtil::Split(const std::string &input, const std::string &split) -> std::vector<std::string> {
+  std::vector<std::string> splits;
+
+  size_t last = 0;
+  size_t input_len = input.size();
+  size_t split_len = split.size();
+  while (last <= input_len) {
+    size_t next = input.find(split, last);
+    if (next == std::string::npos) {
+      next = input_len;
+    }
+
+    // Push the substring [last, next) on to splits
+    std::string substr = input.substr(last, next - last);
+    if (!substr.empty()) {
+      splits.push_back(substr);
+    }
+    last = next + split_len;
+  }
+
+  return splits;
+}
+
+void StringUtil::RTrim(std::string *str) {
+  // remove trailing ' ', \f, \n, \r, \t, \v
+  str->erase(std::find_if(str->rbegin(), str->rend(), [](int ch) { return std::isspace(ch) == 0; }).base(), str->end());
+}
+
+auto StringUtil::Indent(int num_indent) -> std::string { return std::string(num_indent, ' '); }  // NOLINT
+
+auto StringUtil::Strip(const std::string &str, char c) -> std::string {
+  // There's a copy here which is wasteful, so don't use this in performance-critical code!
+  std::string tmp = str;
+  tmp.erase(std::remove(tmp.begin(), tmp.end(), c), tmp.end());
+  return tmp;
+}
+
+auto StringUtil::Replace(std::string source, const std::string &from, const std::string &to) -> std::string {
+  uint64_t start_pos = 0;
+  while ((start_pos = source.find(from, start_pos)) != std::string::npos) {
+    source.replace(start_pos, from.length(), to);
+    start_pos += to.length();  // In case 'to' contains 'from', like
+                               // replacing 'x' with 'yx'
+  }
+
+  return source;
 }
 
 }  // namespace bustub
--- src/include/common/util/string_util.h
+++ src/include/common/util/string_util.h
@@ -82,5 +82,62 @@ class StringUtil {
    * @return bytes formatted into the appropriate kilobyte, megabyte or gigabyte representation
    */
   static auto FormatSize(uint64_t bytes) -> std::string;
+
+  /**
+   * @return string wrapped with control characters to appear bold in the console
+   */
+  static auto Bold(const std::string &str) -> std::string;
+
+  /**
+   * @return uppercase version of the string
+   */
+  static auto Upper(const std::string &str) -> std::string;
+
+  /**
+   * @return lower version of the string
+   */
+  static auto Lower(const std::string &str) -> std::string;
+
+  /**
+   * @return string formatted with printf semantics
+   */
+  static auto Format(std::string fmt_str, ...) -> std::string;
+
+  /** @return
+   * input string split based on the split string
+   */
+  static auto Split(const std::string &input, const std::string &split) -> std::vector<std::string>;
+
+  /**
+   * Removes the whitespace characters from the right side of the string.
+   * @param[in,out] str string to be trimmed on the right
+   */
+  static void RTrim(std::string *str);
+
+  /**
+   * @return indented string
+   */
+  static auto Indent(int num_indent) -> std::string;
+
+  /**
+   * Return a new string that has stripped all occurrences of the provided character from the provided string.
+   *
+   * NOTE: WASTEFUL. Performs a copy. Do NOT use for performance-critical code!
+   *
+   * @param str input string
+   * @param c character to be removed
+   * @return a new string with no occurrences of the provided character
+   */
+  static auto Strip(const std::string &str, char c) -> std::string;
+
+  /**
+   * Replace parts of the string from `from` to `to`.
+   *
+   * @param source input string
+   * @param from substring to be searched
+   * @param to replace `from` to `to`
+   * @return a new string with all occurrences of `from` replaced with `to`.
+   */
+  static auto Replace(std::string source, const std::string &from, const std::string &to) -> std::string;
 };
 }  // namespace bustub
